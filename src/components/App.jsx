51dba28a2ec893e037933f772fa08e6ac457713a
Anastasia Slezko <slezkoanastasiia@gmail.com>
2022-07-21 12:49:41 +0300

    react/components/second-sample
    second-sample/completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import Profile from "./Profile/Profile";
-import user from "../components/user.json";
+import user from "../data/user.json";
 import Statics from "./Static/Static";
-import stats from "../components/data.json";
-import friends from "../components/friends.json";
+import stats from "../data/data.json";
+import friends from "../data/friends.json";
 import FriendList from "../components/FriendList/FriendList";
-import transactions from "../components/transactions.json";
+import transactions from "../data/transactions.json";
 import TransactionHystory from "./Transactions/TransactionHystory";
 import Section from "./Sections/sections";
 export default function App() {
   return (
     <div>
-      <Section title={"Task-1 'Social network profile'"}>
+      <Section title="Task-1 'Social network profile'">
         <Profile
           avatar={user.avatar}
           username={user.username}
@@ -23,13 +23,13 @@ export default function App() {
           quantity={user.stats.quantity}
         />
       </Section>
-      <Section title={"Task-2 'Statistics section'"}>
+      <Section title="Task-2 'Statistics section'">
         <Statics stats={stats} />
       </Section>
       <Section>
         <FriendList friends={friends} />
       </Section>
-      <Section title={"Task-4 'Transaction history'"}>
+      <Section title="Task-4 'Transaction history'">
         <TransactionHystory items={transactions} />
       </Section>
     </div>
